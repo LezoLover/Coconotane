5eb7e418af7719e5f71b1e6cb49855f23a9b8d8b
Franco Garcia <72900907+LezoLover@users.noreply.github.com>
2024-11-18 19:16:49 -0600

    Update page.tsx

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -29,7 +29,7 @@ export default function Contacto() {
     setIsSubmitting(true);
 
     try {
-      const docRef = await addDoc(collection(db, "/Mensajes"), {
+      const _docRef = await addDoc(collection(db, "/Mensajes"), {
         ...formData,
         timestamp: new Date(), // Puedes agregar un timestamp para saber cuándo se envió
       });
