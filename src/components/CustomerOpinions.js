f61a414688e541472820b4d42c837d6788408b76
Franco Garcia <72900907+LezoLover@users.noreply.github.com>
2024-11-18 18:57:27 -0600

    Update CustomerOpinions.js
    Correciones

--- src/components/CustomerOpinions.js
+++ src/components/CustomerOpinions.js
@@ -2,11 +2,14 @@
 import { useEffect, useState } from "react";
 import { db } from "../utils/firebaseConfig";
 import { collection, getDocs } from "firebase/firestore";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination, Autoplay } from "swiper/modules";
 
+// Aqui tmabien modificamos el codigo
 export default function CustomerOpinions() {
   const [comments, setComments] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
 
   // Obtener datos desde Firestore
   useEffect(() => {
@@ -23,67 +26,52 @@ export default function CustomerOpinions() {
     fetchComments();
   }, []);
 
-  // Cambiar comentario mostrado
-  const nextComment = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % comments.length);
-  };
-
-  const prevComment = () => {
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + comments.length) % comments.length
-    );
-  };
-
   if (comments.length === 0) {
     return <p className="text-center text-gray-700">Cargando comentarios...</p>;
   }
 
-  const currentComment = comments[currentIndex];
-
   return (
-    <section className="text-center py-8">
+    <section className="text-center py-8 px-4">
       <h2 className="text-2xl text-gray-700 font-bold mb-4">Lo que dicen de nosotros...</h2>
-      <div className="relative flex items-center justify-center">
-        {/* Botón de flecha izquierda */}
-        <button
-          className="absolute left-0 text-gray-700 hover:text-blue-500"
-          onClick={prevComment}
-        >
-          <ChevronLeftIcon className="h-8 w-8" />
-        </button>
-
-        {/* Contenedor del comentario */}
-        <div
-          className="w-full max-w-screen-lg p-8 bg-white border rounded-lg shadow-lg"
-          style={{ minWidth: "500px", maxWidth: "600px", maxHeight: "350px" }}
-        >
-          <h3 className="text-xl text-gray-700 font-bold">
-            {currentComment.nombre} {currentComment.apellido}
-          </h3>
-          <p className="text-gray-700 mt-4 text-lg">{currentComment.comentario}</p>
-          {/* Mostrar estrellas según el rate */}
-          <div className="flex justify-center mt-4">
-            {Array(currentComment.rate)
-              .fill()
-              .map((_, i) => (
-                <span key={i} className="text-yellow-500 text-2xl">★</span>
-              ))}
-            {Array(5 - currentComment.rate)
-              .fill()
-              .map((_, i) => (
-                <span key={i + currentComment.rate} className="text-gray-300 text-2xl">★</span>
-              ))}
-          </div>
-        </div>
-
-        {/* Botón de flecha derecha */}
-        <button
-          className="absolute right-0 text-gray-700 hover:text-blue-500"
-          onClick={nextComment}
-        >
-          <ChevronRightIcon className="h-8 w-8" />
-        </button>
-      </div>
+      <Swiper
+        modules={[Pagination, Autoplay]}
+        autoplay={{ delay: 4000, disableOnInteraction: false }}
+        pagination={{ clickable: true }}
+        spaceBetween={20}
+        slidesPerView={1}
+        className="w-full max-w-[300px] sm:max-w-[400px] md:max-w-[600px] max-h-[500px]" // Cambios para responsividad
+      >
+        {comments.map((comment) => (
+          <SwiperSlide key={comment.id} className="h-full w-full">
+            <div className="p-6 bg-white border rounded-lg shadow-lg overflow-hidden h-full w-full flex flex-col justify-center">
+              <h3 className="text-xl text-gray-700 font-bold">
+                {comment.nombre} {comment.apellido}
+              </h3>
+              <p className="text-gray-700 mt-4 text-lg">{comment.comentario}</p>
+              <p className="text-gray-400 mt-4 text-lg">
+                {comment.fecha.toDate().toLocaleDateString("es-ES", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}
+              </p>
+              {/* Mostrar estrellas según el rate */}
+              <div className="flex justify-center mt-4">
+                {Array(comment.rate)
+                  .fill()
+                  .map((_, i) => (
+                    <span key={i} className="text-yellow-500 text-2xl">★</span>
+                  ))}
+                {Array(5 - comment.rate)
+                  .fill()
+                  .map((_, i) => (
+                    <span key={i + comment.rate} className="text-gray-300 text-2xl">★</span>
+                  ))}
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
   );
 }
